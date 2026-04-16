a4ac5fa883b607bb6f13ce99aec01f39ced83064
Karl Vanderslice <karl@karlvanderslice.com>
2026-04-15 23:01:27 -0400

    build: replace serokell hooks with cachix git-hooks.nix and treefmt-nix
    - Replace serokell nix-pre-commit-hooks with cachix git-hooks.nix
    - Add treefmt-nix with alejandra and terraform formatters
    - Replace detect-secrets with gitleaks
    - Add markdownlint-cli2, yamllint, shellcheck hooks
    - Keep checkov as custom hook
    - Add .markdownlint-cli2.yaml and .yamllint

--- flake.nix
+++ flake.nix
@@ -4,113 +4,120 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks = {
-      url = "github:serokell/nix-pre-commit-hooks";
-      flake = false;
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    git-hooks,
+    treefmt-nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs = {
+          alejandra.enable = true;
+          terraform.enable = true;
         };
+      };
 
-        mkNoopTool = name: pkgs.writeShellScriptBin name "exit 0";
-
-        serokellRun = import (pre-commit-hooks + "/nix/run.nix") {
-          tools = {
-            hlint = mkNoopTool "hlint";
-            ormolu = mkNoopTool "ormolu";
-            cabal-fmt = mkNoopTool "cabal-fmt";
-            canonix = mkNoopTool "canonix";
-            nixpkgs-fmt = mkNoopTool "nixpkgs-fmt";
-            shellcheck = pkgs.shellcheck;
-            "elm-format" = mkNoopTool "elm-format";
+      preCommitCheck = git-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
+          shellcheck = {
+            enable = true;
+            types_or = ["bash" "shell"];
           };
-          pre-commit = pkgs.pre-commit;
-          git = pkgs.git;
-          runCommand = pkgs.runCommand;
-          writeText = pkgs.writeText;
-          writeScript = pkgs.writeShellScript;
+          markdownlint-cli2 = {
+            enable = true;
+            name = "markdownlint-cli2";
+            entry = "${pkgs.markdownlint-cli2}/bin/markdownlint-cli2";
+            language = "system";
+            files = "\\.md$";
+          };
+          yamllint = {
+            enable = true;
+            settings.configuration = ''
+              ---
+              extends: relaxed
+              rules:
+                line-length: disable
+            '';
+          };
+          gitleaks = {
+            enable = true;
+            name = "gitleaks";
+            entry = "${pkgs.gitleaks}/bin/gitleaks detect --no-banner --redact --source .";
+            language = "system";
+            pass_filenames = false;
+          };
+          checkov = {
+            enable = true;
+            name = "checkov";
+            entry = "${pkgs.checkov}/bin/checkov -d . --config-file .checkov.yaml";
+            language = "system";
+            pass_filenames = false;
+            files = "\\.tf$";
+          };
         };
+      };
+    in {
+      formatter = treefmtEval.config.build.wrapper;
 
-        localPreCommitHooks = pkgs.writeText "pre-commit-hooks" ''
-          -   id: terraform-fmt
-              name: terraform fmt
-              entry: ${pkgs.terraform}/bin/terraform fmt -check -recursive
-              language: system
-              files: '\\.tf$'
+      checks = {
+        pre-commit = preCommitCheck;
+        formatting = treefmtEval.config.build.check self;
 
-          -   id: checkov
-              name: checkov
-              entry: ${pkgs.checkov}/bin/checkov -d . --config-file .checkov.yaml
-              language: system
-              pass_filenames: false
-              files: '\\.tf$'
-
-          -   id: detect-secrets
-              name: detect secrets
-              entry: ${pkgs.detect-secrets}/bin/detect-secrets-hook --exclude-files '(^|/)(\\.terraform\\.lock\\.hcl|terraform\\.tfvars\\.example|terraform\\.tfvars|terraform\\.tfstate(\\..*)?)$'
-              language: system
+        terraform-fmt = pkgs.runCommand "terraform-fmt-check" {
+          src = self;
+          nativeBuildInputs = [pkgs.terraform];
+        } ''
+          cp -R "$src" repo
+          chmod -R u+w repo
+          cd repo
+          terraform fmt -check -recursive .
+          touch "$out"
         '';
 
-        preCommitCheck = serokellRun {
-          src = ./.;
-          hooks = localPreCommitHooks;
-        };
-      in {
-        checks = {
-          terraform-fmt = pkgs.runCommand "terraform-fmt-check" {
-            src = self;
-            nativeBuildInputs = [ pkgs.terraform ];
-          } ''
-            cp -R "$src" repo
-            chmod -R u+w repo
-            cd repo
-            terraform fmt -check -recursive .
-            touch "$out"
-          '';
+        checkov = pkgs.runCommand "checkov-scan" {
+          src = self;
+          nativeBuildInputs = [pkgs.checkov];
+        } ''
+          cp -R "$src" repo
+          chmod -R u+w repo
+          cd repo
+          export HOME="$TMPDIR"
+          checkov -d . --config-file .checkov.yaml
+          touch "$out"
+        '';
+      };
 
-          checkov = pkgs.runCommand "checkov-scan" {
-            src = self;
-            nativeBuildInputs = [ pkgs.checkov ];
-          } ''
-            cp -R "$src" repo
-            chmod -R u+w repo
-            cd repo
-            export HOME="$TMPDIR"
-            checkov -d . --config-file .checkov.yaml
-            touch "$out"
-          '';
-
-          secret-scan = pkgs.runCommand "detect-secrets-scan" {
-            src = self;
-            nativeBuildInputs = [ pkgs.git pkgs.detect-secrets ];
-          } ''
-            cp -R "$src" repo
-            chmod -R u+w repo
-            cd repo
-            git init -q
-            git add .
-            detect-secrets-hook \
-              --exclude-files '(^|/)(\.terraform\.lock\.hcl|terraform\.tfvars\.example|terraform\.tfvars|terraform\.tfstate(\..*)?)$' \
-              $(git ls-files)
-            touch "$out"
-          '';
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            terraform
-            checkov
-            pre-commit
-            detect-secrets
-            just
-          ];
-          shellHook = preCommitCheck.shellHook;
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        inherit (preCommitCheck) shellHook;
+        packages = with pkgs; [
+          terraform
+          checkov
+          gitleaks
+          just
+          markdownlint-cli2
+          shellcheck
+          yamllint
+        ];
+      };
+    });
 }
